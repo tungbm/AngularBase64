e061dfb4a5f8c3298a880ec7844f0c60a90944eb
tungbm <tungbm@pitagon.vn>
2022-03-04 15:37:03 +0700

    z

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ConvertDataComponent } from './convert-data/convert-data.component';
+import { ConvertImgBase64Component } from './convert-img-base64/convert-img-base64.component';
+import { ConvertBase64ImgComponent } from './convert-base64-img/convert-base64-img.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConvertDataComponent,
+    ConvertImgBase64Component,
+    ConvertBase64ImgComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
